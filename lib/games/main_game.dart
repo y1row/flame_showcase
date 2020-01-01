6b297009e5f7b40b7f2288889d615d2dd8a6d74d
y1row <y1row.kato@gmail.com>
2020-01-01 16:00:19 +0900

    メインループ

---
+++ lib/games/main_game.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class MainGame extends Game {
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  void update(double t) {
+    // TODO: implement update
+  }
+}
