748ce62132a746f8e2302b3536c55e842cfb9606
y1row <y1row.kato@gmail.com>
2020-01-01 16:10:21 +0900

    白い箱を描画

--- lib/games/main_game.dart
+++ lib/games/main_game.dart
@@ -3,8 +3,32 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 
 class MainGame extends Game {
+
+  Size screenSize;
+
+  @override
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
+  }
+
   void render(Canvas canvas) {
-    // TODO: implement render
+    var bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    var bgPaint = Paint();
+    bgPaint.color = Color(0xff000000);
+    canvas.drawRect(bgRect, bgPaint);
+
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect = Rect.fromLTWH(
+        screenCenterX - 75,
+        screenCenterY - 75,
+        150,
+        150
+    );
+    var boxPaint = Paint();
+    boxPaint.color = Color(0xffffffff);
+    canvas.drawRect(boxRect, boxPaint);
   }
 
   void update(double t) {
