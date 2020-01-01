f5f1cc42f6a0e444387b79da2d55bda065a000a8
y1row <y1row.kato@gmail.com>
2020-01-01 20:43:50 +0900

    敵のスポーン

--- lib/games/main_game.dart
+++ lib/games/main_game.dart
@@ -1,13 +1,17 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame_showcase/components/backyard.dart';
+import 'package:flame_showcase/components/fly.dart';
 import 'package:flutter/gestures.dart';
 
 class MainGame extends Game {
   Size screenSize;
   double tileSize;
+  Random rnd;
+  List<Fly> flies;
   Backyard background;
 
   MainGame() {
@@ -15,9 +19,12 @@ class MainGame extends Game {
   }
 
   void initialize() async {
+    flies = List<Fly>();
     resize(await Flame.util.initialDimensions());
+    rnd = Random();
 
     background = Backyard(this);
+    spawnFly();
   }
 
   @override
@@ -29,11 +36,25 @@ class MainGame extends Game {
 
   void render(Canvas canvas) {
     background.render(canvas);
+    flies.forEach((Fly fly) => fly.render(canvas));
   }
 
   void update(double t) {
-    // TODO: implement update
+    flies.forEach((Fly fly) => fly.update(t));
+    flies.removeWhere((Fly fly) => fly.isOffScreen);
   }
 
-  void onTapDown(TapDownDetails d) {}
+  void spawnFly() {
+    double x = rnd.nextDouble() * (screenSize.width - tileSize);
+    double y = rnd.nextDouble() * (screenSize.height - tileSize);
+    flies.add(Fly(this, x, y));
+  }
+
+  void onTapDown(TapDownDetails d) {
+    flies.forEach((Fly fly) {
+      if (fly.flyRect.contains(d.globalPosition)) {
+        fly.onTapDown();
+      }
+    });
+  }
 }
